cbd844d9364ba832b35a2c599d9d5ff2256ecd59
Mohammad Bagher Soltani <m.soltani.252@gmail.com>
2023-07-23 16:40:32 +0330

    minor change

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ad-utils",
-    py_modules=["ad_utils"],
-    install_requires=find_packages(),
+    name="improved-diffusion",
+    py_modules=["improved_diffusion"],
+    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
+
+# setup(
+#     name="ad-utils",
+#     py_modules=["ad_utils"],
+#     install_requires=find_packages(),
+# )
